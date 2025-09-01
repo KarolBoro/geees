83733f4a52352bb151284a4f2e03ed34918b185d
Darrmen <114183766+KarolBoro@users.noreply.github.com>
2025-09-01 18:27:17 +0200

    update

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
+from datetime import datetime
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 try:
     client.connect(("localhost", 5050))
@@ -11,10 +13,12 @@ done = False
 nazwa_uzytkownika = input("Podaj nazwe uzytkownika: ")
 client.send(nazwa_uzytkownika.encode('utf-8'))
 while not done:
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     client.send(input("Wiadomosc: ").encode('utf-8'))
     msg = client.recv(1024).decode('utf-8')
+    with open("logs.txt", "a") as f:
+        f.write(time + " | " + msg + "\n")
     if msg == "quit":
-        done = True
         print("Koniec wymiany wiadomości.")
         break
     else:
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-from datetime import  datetime
+from datetime import datetime
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -12,25 +12,24 @@ nazwa_uzytkownika = client.recv(1024).decode('utf-8')
 print(f"Nowy uzytkownik polaczony: {nazwa_uzytkownika}")
 
 done = False
-time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+with open("logs.txt", "a") as f:
+    f.write("\n")
 
 while not done:
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     msg = client.recv(1024).decode('utf-8')
-    if msg == "quit":
-        done = True
-        print("Koniec wymiany wiadomości.")
-        with open("logs.txt", "a") as f:
-            f.write(time + " " + msg + "\n")
-        break
+    with open("logs.txt", "a") as f:
+        f.write(time + " | " + nazwa_uzytkownika + " | " + msg + "\n")
 
+    if msg == "quit":
+        print("Koniec wymiany wiadomości.")
+        break
     else:
         print(f"{nazwa_uzytkownika}: {msg}")
     client.send(f"Server: {input('Twoja odpowiedz: ')}".encode('utf-8'))
-    with open("logs.txt", "a") as f:
-        f.write(time + " " + nazwa_uzytkownika + " " + msg + "\n")
 
 client.close()
 server.close()
 
 # dodac nicki do logow, i zebasdasy tez zeby zapisywal to co odbieramy od uzytkownika safasdfasdf
-# nnnn
