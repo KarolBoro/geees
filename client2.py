e93443e84fff02cd41ffe35ee2c4fe67fc8ce7cb
Darrmen <114183766+KarolBoro@users.noreply.github.com>
2025-09-03 00:37:01 +0200

    czytelniejsze komunikaty, czytelniejszy czat, zmiana listowan, dodana lista skladania v2

--- client2.py
+++ client2.py
@@ -29,35 +29,3 @@ while True:
         break
 
 client.close()
-
-
-
-
-
-
-
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#
-#
-# try:
-#     client.connect(("localhost", 5050))
-# except:
-#     print("Przypal z serwerem :<")
-#
-# done = False
-# nazwa_uzytkownika = input("Podaj nazwe uzytkownika: ")
-# client.send(nazwa_uzytkownika.encode('utf-8'))
-# while not done:
-#     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     client.send(input("Wiadomosc: ").encode('utf-8'))
-#     msg = client.recv(1024).decode('utf-8')
-#     with open("logs.txt", "a") as f:
-#         f.write(time + " | " + msg + "\n")
-#     if msg.strip() == "quit":
-#         print("Koniec wymiany wiadomości.")
-#         break
-#     else:
-#         print(msg)
-#
-# client.close()
--- server.py
+++ server.py
@@ -55,38 +55,3 @@ def start_server():
 
 start_server()
 
-
-
-
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# server.bind(("localhost", 5050))
-#
-# server.listen()
-#
-# client, addr = server.accept()
-# nazwa_uzytkownika = client.recv(1024).decode('utf-8')
-# print(f"Nowy uzytkownik polaczony: {nazwa_uzytkownika}")
-#
-# done = False
-#
-# with open("logs.txt", "a") as f:
-#     f.write("\n")
-#
-# while not done:
-#     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     msg = client.recv(1024).decode('utf-8')
-#     with open("logs.txt", "a") as f:
-#         f.write(time + " | " + nazwa_uzytkownika + " | " + msg + "\n")
-#
-#     if msg.strip() == "quit":
-#         print("Koniec wymiany wiadomości.")
-#         break
-#     else:
-#         print(f"{nazwa_uzytkownika}: {msg}")
-#     client.send(f"Server: {input('Twoja odpowiedz: ')}".encode('utf-8'))
-#
-# client.close()
-# server.close()
-
-# dodac nicki do logow, i zebasdasy tez zeby zapisywal to co odbieramy od uzytkownika safasdfasdf
